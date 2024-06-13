5d6104de8e7b873caaeb04461baac700cd1fe6b1
NicolasAdell <nicolas.fabian2005@gmail.com>
2024-06-13 10:36:36 -0300

    fix: arreglar bucle

--- actividades_python/ej_03/main.py
+++ actividades_python/ej_03/main.py
@@ -5,14 +5,13 @@ bz = Buzzer(22)
 led_rojo = LED(19)
 led_verde = LED(26)
 led_azul = LED(13)
-
-comando = input("Ingrese comando: buzz o rgb: ")
-if comando == "rgb":
-	opcion = input("Ingrese opcion: red, green or blue: ")
-elif comando == "buzz":
-	opcion = input("Ingrese opcion: on u off: ")		
-
 while True:
+	comando = input("Ingrese comando: buzz o rgb: ")
+	if comando == "rgb":
+		opcion = input("Ingrese opcion: red, green or blue: ")
+	elif comando == "buzz":
+		opcion = input("Ingrese opcion: on u off: ")		
+
 	if comando == "buzz":
 		if opcion == "on":
 			bz.on()
